d0969948599a6287760416721f2137d77e61b3df
Prudhvi Raj Mukiri <prudhvirajmukiri@gmail.com>
2025-03-03 23:03:14 +0530

    fix: pipeline issues

--- run_ui.sh
+++ run_ui.sh
@@ -2,7 +2,7 @@
 npm run run_ui_tests
 
 # run allure report generation
-allure generate ./allure-results -o ./allure-report
+allure generate --clean ./allure-results -o ./allure-report
 
 # run allure report open
 #allure open ./allure-report
