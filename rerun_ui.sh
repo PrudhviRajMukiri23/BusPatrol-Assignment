7fcb64d708d50dc66eac712009cee43a48a2e7a2
Prudhvi Raj Mukiri <prudhvirajmukiri@gmail.com>
2025-03-03 21:11:11 +0530

    fix: enabled pipeline yml

---
+++ rerun_ui.sh
@@ -0,0 +1,9 @@
+
+# re-run functional test cases
+npm run rerun_ui_tests
+
+# run allure report generation
+allure generate ./allure-results -o ./allure-report
+
+# run allure report open
+allure open ./allure-report
