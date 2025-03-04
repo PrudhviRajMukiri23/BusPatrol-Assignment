7fef82b513e2dfdfbbc48c921c60bdb3d15d8a40
Prudhvi Raj Mukiri <prudhvirajmukiri@gmail.com>
2025-03-04 07:33:49 +0530

    fix: removed allure reports functionality

--- run_ui.sh
+++ run_ui.sh
@@ -1,8 +1,2 @@
 # run functional test cases
 npm run run_ui_tests
-
-# run allure report generation
-allure generate --clean ./allure-results -o ./allure-report
-
-# run allure report open
-#allure open ./allure-report
