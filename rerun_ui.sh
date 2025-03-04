45020b2d222d69c59bee59ff5943d63c3d41d00b
Prudhvi Raj Mukiri <92009135+PrudhviRajMukiri23@users.noreply.github.com>
2025-03-04 08:33:41 +0530

    Feature/prudhvi adhoc (#8)
    * feat: added the minimal portion of readme
    
    * fix: fixed the failing code
    
    * fix: updated the git ignore file
    
    * fix: updated readme files
    
    * fix: removed the env value
    
    * fix: updated the readme file
    
    * feat: inplemented first ui test case
    
    * fix: updated readme
    
    * feat: added the sh run for ui
    
    * feat: implemented 2test case
    
    * feat: finished the 2 test cases
    
    * feat: added the logs and test cases
    
    * feat: added 3 test cases
    
    * fix: enabled pipeline yml
    
    * fix: updated the yml file
    
    * fis: fixed pipeline code
    
    * fix: changed env value
    
    * chore: commente allure
    
    * fix: xpath issue
    
    * fix: pipeline issues
    
    * Update pipeline.yml
    
    * fix: updated artifact
    
    * fix: removed allure reports functionality
    
    * fix: Implemented the reports feature
    
    * fix: changed the artifact names
    
    * fix: fix ui report gen probled

--- rerun_ui.sh
+++ rerun_ui.sh
@@ -1,3 +1,6 @@
 
 # re-run functional test cases
 npm run rerun_ui_tests
+
+# run report generation
+npm run run_ui_report_gen
--- run_ui.sh
+++ run_ui.sh
@@ -1,2 +1,5 @@
 # run functional test cases
 npm run run_ui_tests
+
+# run report generation
+npm run run_ui_report_gen
